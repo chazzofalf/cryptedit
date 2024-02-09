773bd1a57b37fc7266684303c663cd94e06da6eb
Charles Montgomery <charles.montgomery@charter.net>
2024-02-09 10:13:24 -0600

    Some fixes

--- cryptedit.py
+++ cryptedit.py
@@ -13,6 +13,9 @@ import Cryptodome.Random
 import Cryptodome.Protocol.KDF
 import io
 import shutil
+import traceback
+import stat
+import subprocess
 salt=b'VRv\x17~\xf7\xbd\x95v\x04I\xbbH\xd6L!Eb\xfd\x90{\xa3\xf8L\xbaS\x0b\xfd\xf9\x10JU'
 def check_auth(p:str,filepath:pathlib.Path):
     return True
@@ -34,7 +37,7 @@ def save_file(textfile:pathlib.Path,cryptfile:pathlib.Path,p:str):
                     compress_bytes=zstd.compress(block,22)                                    
                     blockmemfile=io.BytesIO()
                     cipher = Cryptodome.Cipher.AES.new(key,Cryptodome.Cipher.AES.MODE_EAX)
-                    ciphered_block,tag = cipher(compress_bytes)
+                    ciphered_block,tag = cipher.encrypt_and_digest(compress_bytes)
                     blockmemfile.write(cipher.nonce)
                     blockmemfile.write(tag)
                     blockmemfile.write(ciphered_block)
@@ -54,8 +57,10 @@ def save_file(textfile:pathlib.Path,cryptfile:pathlib.Path,p:str):
                     sizedcblock.extend(cryptblock)
                     sizedcblock=bytearray(sizedcblock)
                     cryptio.write(sizedcblock)
+                    block=textio.read(buffsize)
         success=True
-    except:
+    except Exception as e:
+        print('\n'.join(traceback.format_exception(e)))
         print('Unable to write to file.')
     return success
 def load_file(textfile:pathlib.Path,cryptfile:pathlib.Path,p:str):
@@ -66,7 +71,7 @@ def load_file(textfile:pathlib.Path,cryptfile:pathlib.Path,p:str):
             with open(str(cryptfile),'rb') as cryptio:
                 key=Cryptodome.Protocol.KDF.PBKDF2(p,salt,dkLen=32)    
                 blocksizebytes=cryptio.read(4)
-                while len(blocksizebytes) == 4:
+                while len(blocksizebytes) > 0:
                     blocklen=0                    
                     for f in reversed(range(0,4)):
                         f_inv=3-f
@@ -77,7 +82,7 @@ def load_file(textfile:pathlib.Path,cryptfile:pathlib.Path,p:str):
                     if len(block) == blocklen:
                         ramblockfile=io.BytesIO(block)
                         nonce=ramblockfile.read(16)
-                        tag=ramblockfile
+                        tag=ramblockfile.read(16)
                         ciphered_block=ramblockfile.read()
                         cipher=Cryptodome.Cipher.AES.new(key,Cryptodome.Cipher.AES.MODE_EAX,nonce)
                         decrypted_block=cipher.decrypt_and_verify(ciphered_block,tag)
@@ -87,8 +92,9 @@ def load_file(textfile:pathlib.Path,cryptfile:pathlib.Path,p:str):
                         raise Exception('Incomplete block! Partial File!')
                     blocksizebytes=cryptio.read(4)                
         success=True
-    except:
-        print('Unable to write to file.')
+    except Exception as e:
+        print('\n'.join(traceback.format_exception(e)))
+        print('Unable to read from file.')
     return success
 def main():
     valid_args=False
@@ -105,7 +111,7 @@ def main():
                 auth_success=p==p2                
             else:
                 new_file=False
-                p=getpass('Enter Passphrase: ')
+                p=getpass.getpass('Enter Passphrase: ')
                 auth_success=check_auth(p=p,filepath=filepath)
         else:
             valid_file=False
@@ -139,18 +145,20 @@ def main():
                                 with open(str(scriptfile),'w') as scriptout:
                                     scriptout.write(script_text)
                                     
-                                    
-                                os.chmod(str(scriptfile),os.stat.stat.S_IXUSR | os.stat.stat.S_IXUSR | os.stat.stat.S_IXGRP | os.stat.stat.S_IXOTH)
+                                subprocess.run(['ls','-lha',str(tempdir)])
+                                os.chmod(str(scriptfile),stat.S_IEXEC  | stat.S_IXGRP | stat.S_IXOTH | stat.S_IREAD | stat.S_IRGRP | stat.S_IROTH)
+                                subprocess.run(['ls','-lha',str(tempdir)])
                                 pty.spawn([str(scriptfile)],read,read)
                                 editor_success=True
-                            except:
+                            except Exception as e:
+                                print('\n'.join(traceback.format_exception(e)))
                                 print('Unable to get terminal on this system.')
                                 editor_success=False         
                         else:
                             print('Unable to get terminal size.')
                             editor_success=False 
-                    if editor_success and save_file(textfile=textfile,cryptfile=cryptfile):
-                        shutil.copyfile(src=str(textfile),dst=filename)
+                    if editor_success and save_file(textfile=textfile,cryptfile=cryptfile,p=p):
+                        shutil.copyfile(src=str(cryptfile),dst=filename)
                     else:
                         print('Unable to save file. Aborting')
             else:
@@ -159,6 +167,7 @@ def main():
                     termfile=pathlib.Path(tempdir,'term.bin')
                     scriptfile=pathlib.Path(tempdir,'script.sh')
                     cryptfile=pathlib.Path(tempdir,'crypt.bin')
+                    shutil.copyfile(src=filename,dst=str(cryptfile))
                     if load_file(str(textfile),str(cryptfile),p):
                         with open(str(termfile),'wb') as termio: 
                             def read(fd):
@@ -182,17 +191,19 @@ def main():
                                         scriptout.write(script_text)
                                         
                                         
-                                    os.chmod(str(scriptfile),os.stat.stat.S_IXUSR | os.stat.stat.S_IXUSR | os.stat.stat.S_IXGRP | os.stat.stat.S_IXOTH)
+                                    os.chmod(str(scriptfile),stat.S_IEXEC  | stat.S_IXGRP | stat.S_IXOTH | stat.S_IREAD | stat.S_IRGRP | stat.S_IROTH)
+                                    
                                     pty.spawn([str(scriptfile)],read,read)
                                     editor_success=True
-                                except:
+                                except Exception as e:
+                                    print('\n'.join(traceback.format_exception(e)))
                                     print('Unable to get terminal on this system.')
                                     editor_success=False         
                             else:
                                 print('Unable to get terminal size.')
                                 editor_success=False 
-                        if editor_success and save_file(textfile=textfile,cryptfile=cryptfile):
-                            shutil.copyfile(src=str(textfile),dst=filename)
+                        if editor_success and save_file(textfile=textfile,cryptfile=cryptfile,p=p):
+                            shutil.copyfile(src=str(cryptfile),dst=filename)
                         else:
                             print('Unable to save file. Aborting')
                     else:
